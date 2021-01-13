92d57cf32f061f29507c66c0555dbd6c4e4a4304
2KUL4SKUL <77398978+2KUL4SKUL@users.noreply.github.com>
2021-01-13 09:40:13 -0600

    Add files via upload

---
+++ gaaay/Shared/gaaayApp.swift
@@ -0,0 +1,17 @@
+//
+//  gaaayApp.swift
+//  Shared
+//
+//  Created by A.M. Student on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct gaaayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
